ceb0144a21d97e912bab1919a17c236b8e51d469
unknown <iqrat269@gmail.com>
2025-08-30 22:04:06 +0500

    Updated server.js

--- Backend/server.js
+++ Backend/server.js
@@ -1,36 +1,43 @@
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
-const path=require("path")
+const path = require("path");
 
-//dotenv configuaration
+// dotenv config
 dotenv.config();
 
-//res object
+// res object
 const app = express();
 
-//middleware
-app.use(cors(
-   { origin: "https://portfolio-quyyog7l8-iqras-projects-c95e893d.vercel.app",
-      credentials: true
-   }
-));
+// middleware
+app.use(cors({
+   origin: [
+      "http://localhost:3000",
+      "https://portfolio-quyyog7l8-iqras-projects-c95e893d.vercel.app"
+   ],
+   credentials: true
+}));
 app.use(express.json());
 
-//static file
-app.use(express.static(path.join(__dirname, './client/build')))
+// routes
+app.use("/api/v1/portfolio", require("./routes/routes"));
 
-//routes
-app.use('/api/v1/portfolio',require('./routes/routes'))
-app.get((req,res)=>{
-    res.send("run correct")
-})
+// static files
+app.use(express.static(path.join(__dirname, "./client/build")));
 
+// test route
+app.get("/", (req, res) => {
+   res.send("run correct");
+});
 
+// react fallback
+app.get("*", (req, res) => {
+   res.sendFile(path.resolve(__dirname, "./client/build/index.html"));
+});
 
 const PORT = process.env.PORT || 3000;
 
-//listning
-app.listen(PORT,()=>{
-    console.log(`node server is running at http://localhost:${PORT}`)
-})
+// listening
+app.listen(PORT, () => {
+   console.log(`node server is running at http://localhost:${PORT}`);
+});
