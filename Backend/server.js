c0aee1d1650046f298876d9144830ecc8e8aad8d
unknown <iqrat269@gmail.com>
2025-08-30 20:26:12 +0500

    Removed nested git repo from client

---
+++ Backend/server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const path=require("path")
+
+//dotenv configuaration
+dotenv.config();
+
+//res object
+const app = express();
+
+//middleware
+app.use(cors(
+   { origin: "http://localhost:3000"}
+));
+app.use(express.json());
+
+//static file
+app.use(express.static(path.join(__dirname, './client/build')))
+
+//routes
+app.use('/api/v1/portfolio',require('./routes/routes'))
+app.get('*',(req,res)=>{
+    res.sendFile(path.join(__dirname,'./client/build/index.html'))
+})
+
+
+const PORT = process.env.PORT || 3000;
+
+//listning
+app.listen(PORT,()=>{
+    console.log(`node server is running at http://localhost:${PORT}`)
+})
